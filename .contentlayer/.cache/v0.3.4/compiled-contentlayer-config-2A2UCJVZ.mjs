f3d0dbfa8c4d865f6df3644d97e124d484665143
hanna <hannawkozak@gmail.com>
2025-06-18 18:24:45 +0100

    project setup

---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-2A2UCJVZ.mjs
@@ -0,0 +1,23 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `posts/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    description: { type: "string", required: true },
+    slug: { type: "string", required: true },
+    date: { type: "date", required: true },
+    layout: { type: "string", required: false }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "content",
+  documentTypes: [Post]
+});
+export {
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-2A2UCJVZ.mjs.map
---
+++ contentlayer.config.ts
@@ -0,0 +1,19 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+
+export const Post = defineDocumentType(() => ({
+	name: 'Post',
+	filePathPattern: `posts/*.mdx`,
+	contentType: 'mdx',
+	fields: {
+		title: { type: 'string', required: true },
+		description: { type: 'string', required: true },
+		slug: { type: 'string', required: true },
+		date: { type: 'date', required: true },
+		layout: { type: 'string', required: false },
+	},
+}));
+
+export default makeSource({
+	contentDirPath: 'content',
+	documentTypes: [Post],
+});
